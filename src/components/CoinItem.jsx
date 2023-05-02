44c79a87ec62441af69b33c3a516520ee34e6dd6
Rene Groothuis <renegrooth@gmail.com>
2023-05-02 19:42:00 +1200

    added links and style

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AiOutlineStar } from 'react-icons/ai'
 import { Sparklines, SparklinesLine } from 'react-sparklines'
+import { Link } from 'react-router-dom'
 
 const CoinItem = ({ coin }) => {
   return (
@@ -11,14 +12,16 @@ const CoinItem = ({ coin }) => {
       <td>{coin.market_cap_rank}</td>
 
       <td>
-        <div className="flex items-center">
-          <img
-            className="w-6 mr-2 rounded-full"
-            src={coin.image}
-            alt={coin.name}
-          />
-          <p className="hidden sm:table-cell">{coin.name}</p>
-        </div>
+        <Link to={`/coin/${coin.id}`}>
+          <div className="flex items-center">
+            <img
+              className="w-6 mr-2 rounded-full"
+              src={coin.image}
+              alt={coin.name}
+            />
+            <p className="hidden sm:table-cell">{coin.name}</p>
+          </div>
+        </Link>
       </td>
       <td>{coin.symbol.toUpperCase()}</td>
       <td>${coin.current_price.toLocaleString()}</td>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { FaTwitter, FaFacebook, FaTiktok, FaGithub } from 'react-icons/fa'
+import ThemeToggle from './ThemeToggle'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
